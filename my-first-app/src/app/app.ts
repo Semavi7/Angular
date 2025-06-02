60358b0a290f04eedce30bdc64a66a704b886c91
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-06-02 15:52:14 +0300

    Angular

--- my-first-app/src/app/app.ts
+++ my-first-app/src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ComponentHome } from './component-home/component-home';
+import { About } from './about/about';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ComponentHome, About, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'my-first-app';
+  value:string = "Hello World!"
+  pClassName: string = "red"
+  clickMe(){
+    alert("beni tıkladın")
+  }
+
+  changeValue(event:any){
+    this.value = event.target.value;
+  }
 }
