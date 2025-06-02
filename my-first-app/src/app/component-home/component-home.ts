019b4bb5923b2f34d8d60fc309d2eb6c8aa4bdb8
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-06-02 14:47:04 +0300

    first commit

---
+++ my-first-app/src/app/component-home/component-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-home',
+  imports: [],
+  templateUrl: './component-home.html',
+  styleUrl: './component-home.css'
+})
+export class ComponentHome {
+
+}
